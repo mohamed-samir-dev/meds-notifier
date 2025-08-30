6d25f15f38670621cd2b623181d460af0218435a
Mohammed Samier <mohammedsamiermouawad@gmail.com>
2025-08-30 11:31:47 +0300

    feat: add service worker for offline functionality

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'medication-reminder-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/style.css',
-    '/script.js',
+    '/assets/css/style.css',
+    '/assets/js/script.js',
     '/manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
